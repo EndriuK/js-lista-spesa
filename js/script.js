189392654136676cbe5206b9774a06211ff3263a
EndriuK <endrit.kaskija@gmail.com>
2024-07-02 15:41:47 +0200

    [UPD] recupero elemento dal dom

--- js/script.js
+++ js/script.js
@@ -1,2 +1,6 @@
 // INIZIALIZZO L'ARRAY
 const ingredients = ["guanciale", "pepe", "pecorino", "uova", "spaghetti"];
+
+// RECUPERO LA MIA LISTA SPESA DAL DOM
+
+const listaSpesa = document.getElementById("lista-spesa");
