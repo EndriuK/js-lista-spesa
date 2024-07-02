17f339587c10ad5d85e150b097631124563aa850
EndriuK <endrit.kaskija@gmail.com>
2024-07-02 15:57:09 +0200

    [UPD] ciclo while

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // INIZIALIZZO L'ARRAY
-const ingredients = ["guanciale", "pepe", "pecorino", "uova", "spaghetti"];
+const ingredienti = ["guanciale", "pepe", "pecorino", "uova", "spaghetti"];
 
 // RECUPERO LA MIA LISTA SPESA DAL DOM
 
@@ -7,3 +7,11 @@ const listaSpesa = document.getElementById("lista-spesa");
 
 // INIZIALIZZO LA VARIABILE DI INDICE
 let i = 0;
+
+// CICLO L'ARRAY CON UN CICLO WHILE
+while (i < ingredienti.length) {
+  const li = document.createElement("li");
+  li.innerText = ingredienti[i];
+  listaSpesa.append(li);
+  i++; // INCREMENTO LA VARIABILE DI INDICE
+}
