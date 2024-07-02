69e65dd85813c15e001c3c3a18de2653a8a8c335
EndriuK <endrit.kaskija@gmail.com>
2024-07-02 15:54:00 +0200

    [UPD] inzializzo la variabile

--- js/script.js
+++ js/script.js
@@ -4,3 +4,6 @@ const ingredients = ["guanciale", "pepe", "pecorino", "uova", "spaghetti"];
 // RECUPERO LA MIA LISTA SPESA DAL DOM
 
 const listaSpesa = document.getElementById("lista-spesa");
+
+// INIZIALIZZO LA VARIABILE DI INDICE
+let i = 0;
